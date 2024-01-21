5a90ed7705739c53fc706fd8d6b582f650f10c02
Ahmet Faruk İloğlu <iloglu.ahmet@gmail.com>
2024-01-21 18:40:56 +0300

    Tweaking for Turret Models

--- Assets/Scripts/UIInterface.cs
+++ Assets/Scripts/UIInterface.cs
@@ -15,30 +15,34 @@ public class UIInterface : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        // control for mobile devices
+        /*
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             if (!Physics.Raycast(ray, out hit))
                 return;
             focusObj = Instantiate(cubeTurret, hit.point, cubeTurret.transform.rotation);
             focusObj.GetComponent<Collider>().enabled = false;
         }
-        else if (focusObj && Input.GetMouseButton(0))
+        else if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             if (!Physics.Raycast(ray, out hit))
                 return;
             focusObj.transform.position = hit.point + new Vector3(0,1,0);
         }
-        else if (focusObj && Input.GetMouseButtonUp(0))
+        else if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)
         {
             RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.CompareTag("Platform"))
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.CompareTag("Platform") && hit.normal.Equals(new Vector3(0,1,0)))
             {
                 hit.collider.gameObject.tag = "Occupied";
+                focusObj.transform.position = new Vector3(hit.collider.gameObject.transform.position.x,
+                    focusObj.transform.position.y, hit.collider.gameObject.transform.position.z);
             }
             else
             {
@@ -46,6 +50,42 @@ public class UIInterface : MonoBehaviour
             }
 
             focusObj = null;
+            */
+        
+            // control for pc 
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (!Physics.Raycast(ray, out hit))
+                    return;
+                focusObj = Instantiate(cubeTurret, hit.point, cubeTurret.transform.rotation);
+                focusObj.GetComponent<Collider>().enabled = false;
+            }
+            else if (focusObj && Input.GetMouseButton(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (!Physics.Raycast(ray, out hit))
+                    return;
+                focusObj.transform.position = hit.point;
+            }
+            else if (focusObj && Input.GetMouseButtonUp(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.CompareTag("Platform") && hit.normal.Equals(new Vector3(0,1,0)))
+                {
+                    hit.collider.gameObject.tag = "Occupied";
+                    focusObj.transform.position = new Vector3(hit.collider.gameObject.transform.position.x,
+                        focusObj.transform.position.y, hit.collider.gameObject.transform.position.z);
+                }
+                else
+                {
+                    Destroy(focusObj);
+                }
+
+                focusObj = null;
         }
     }
 }
